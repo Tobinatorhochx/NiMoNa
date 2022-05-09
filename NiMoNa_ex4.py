1ed6a90cfdf42120323564747367d1bd955c3307
Tobinatorhochx <t.stoffels@aol.com>
2022-05-09 19:44:47 +0200

    second version of exercise 4

--- NiMoNa_ex4.py
+++ NiMoNa_ex4.py
@@ -1,51 +1,59 @@
+# This exercise went pretty wild. The behaviour for A = 1 is cool, but things are starting to get nasty for bigger A. I only 
+# decided to increase A by increments of 2, but I am still reaching populations below 0. I am not sure whether this is my bad 
+# implementation of the model, or the model itself that leads to weird results.
+
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+
 # r returns the replicator
 def r(x, M):
-    #print(M)
-    #print(M.dot(x), x)
     return M.dot(x)/np.dot(x, M.dot(x))
 
-def m(V, C, x):
+# m returns an array containing the matrices for each set of parameters and time
+def m(V, C, x, A):
     M = np.empty((2, 2, len(x), len(V)))
-    for d, v, c, A in zip(np.arange(len(V)), V, C, [x for x in range(1, 2*len(V), 2)]):
+    for d, v, c, a in zip(np.arange(len(V)), V, C, A):
         for t  in range(len(x)):
-            print(v(x[t], A))
-            M[0, 0, t, d] = (v(x[t], A)-c)/2
-            M[0, 1, t, d] = v(x[t], A)
+            M[0, 0, t, d] = (v(x[t], a)-c)/2
+            M[0, 1, t, d] = v(x[t], a)
             M[1, 0, t, d] = 0
-            M[1, 1, t, d] = v(x[t], A)/2
+            M[1, 1, t, d] = v(x[t], a)/2
     return M
 
 
 def main():
 
-    # intitializing the parameters
+    # intitializing the parameters. C is constant, V fluctuating, p contains the populations and M are the matrices.
     C = [1.5 for x in range(5)]
     V = [(lambda t, A: 1 + A * 0.1 * np.sin(t)) for x in range(5)]
+    A = [x for x in range(1, 2*len(V), 2)]
+    x = np.linspace(0, 15, 16)
+    p = np.empty((5, 2, 16))
+    M = m(V, C, x, A)
 
-
-
-    x = np.linspace(0, 15, 15)
-    p = np.empty((5, 2, 15))
     fig, axs = plt.subplots(5, sharex=True)
 
-    M = m(V, C, x)
-
-    
+    # calculating p
     for d in range(len(V)):
         p[d, :, 0] = np.array([0.5, 0.5])
         for i in range(p.shape[2]-1):       
-            p[d, :, i+1] = p[d, :, i] * r(x = p[d, :, i], M = M[:, :, i, d])
+            p[d, :, i+1] = p[d, :, i] * r(x = p[d, :, i], M = M[:, :, i, d])            
+            # normalizing the population
+            p[d, :, i+1] =  p[d, :, i+1]/(sum(np.absolute(p[d, :, i+1]))) 
+
+    # plotting the populations
         axs[d].plot(x, p[d, 0], label="hawks")
         axs[d].plot(x, p[d, 1], label="doves")
-    #print(p)
-    plt.legend()
+        axs[d].set_title("V = 1 + A * sin(x), C = {0}, A = {1}".format(C[d], A[d]))
+    axs[0].legend(loc="upper right")
+    axs[0].set_ylim([0,1])  
+    plt.tight_layout()
     plt.show()
 
-
-main()
+if __name__ == "__main__":
+    main()
 
 
 """
--- testing_ground.py
+++ testing_ground.py
@@ -4,7 +4,7 @@ C = [1.5 for x in range(5)]
 V = [lambda t: 1 + A * 0.1 * np.sin(t) for A in range(1, 1001, 200)]
 
 x = np.linspace(0, 5, 6)
-
+print(np.arange(14).reshape(2, 7)[:, 4], sum(np.arange(14).reshape(2, 7)[:, 4]))
 V = [(lambda t, k: 1 + 0.1 * k*  np.sin(t)) for y in range(2)]
 for f, k in zip(V, [x for x in range(len(V))]):
     print(f(3, k))
