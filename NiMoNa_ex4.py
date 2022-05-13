8abb0704c34e215434abe4c65b819fa159a47eb4
Tobinatorhochx <53875117+Tobinatorhochx@users.noreply.github.com>
2022-05-13 22:49:26 +0200

    Update NiMoNa_ex4.py

--- NiMoNa_ex4.py
+++ NiMoNa_ex4.py
@@ -54,33 +54,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-"""
-
-    for V, C, p_0, p_1, d in [[1., 1.5, 0.5, 0.5, 0],[1.5, 1., 0.5, 0.5, 1],[1., 1.5, 0.1, 0.9, 2],[1., 1.5, 0.9, 0.1, 3]]:
-        # Payoff matrix
-        M = np.array([[(V-C)/2, V], [0,V/2]])
-        
-        # storing the population in a 3D-array
-        p[d, 0:2, 0] = np.array([p_0, p_1])
-        for i in range(p.shape[2]-1):
-        
-            p[d, 0:2, i+1] = p[d, 0:2, i] * r(x = p[d, 0:2, i], M = M)
-    
-    # plotting the function and adjusting parameters
-        axs[d].plot(x, p[d, 0], label = "Hawks") 
-        axs[d].plot(x, p[d, 1], label = "Doves")
-        axs[d].set_title("V = {0}, C = {1}, P_0 = {2} = {3}".format(V, C, p_0, p_1))
-    axs[0].legend(loc="upper right")
-    axs[0].set_ylim([0,1])
-    axs[2].set_ylim([0,1])
-    plt.tight_layout()
-    
-    
-    x = np.linspace(0, 15, 400)
-    y = np.sin(40*x)
-    axs[4].plot(x, y)
-    plt.show()
-"""
-
