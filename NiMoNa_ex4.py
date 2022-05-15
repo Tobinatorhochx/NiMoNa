406f4bcd888f66d9c5fa60d4bd3c3d0e812c7133
Tobinatorhochx <t.stoffels@aol.com>
2022-05-15 17:44:17 +0200

    fourth version

--- NiMoNa_ex4.py
+++ NiMoNa_ex4.py
@@ -11,34 +11,35 @@ import matplotlib.pyplot as plt
 # r returns the replicator
 def r(x, M):
     return M.dot(x)/np.dot(x, M.dot(x))
-
+"""
 # m returns an array containing the matrices for each set of parameters and time
 def m(V, C, x, A):
-    M = np.empty((2, 2, len(x), len(V)))
+    M = np.empty((2, 2, len(x), len(V)), dtype=float)
     for d, v, c, a in zip(np.arange(len(V)), V, C, A):
         for t  in range(len(x)):
             M[0, 0, t, d] = (v(x[t], a)-c)/2
             M[0, 1, t, d] = v(x[t], a)
             M[1, 0, t, d] = 0
             M[1, 1, t, d] = v(x[t], a)/2
+        print("A: {0}, M: {1}\n\n".format(A, M))
     return M
 
 
-def main():
+def main(dim=5):
 
     # intitializing the parameters. C is constant, V fluctuating, p contains the populations and M are the matrices.
-    C = [1.5 for x in range(5)]
-    V = [(lambda t, A: 1 + A * 0.1 * np.sin(t)) for x in range(5)]
+    C = [1.5 for x in range(dim)]
+    V = [(lambda t, A: 1 + A * 0.1 * np.sin(t)) for x in range(dim)]
     A = [x/10 for x in range(1, 10*len(V), 10)]
     # first version: A = [x for x in range(1, 2*len(V), 2)]
     x = np.linspace(0, 15, 16)
-    p = np.empty((5, 2, 16))
+    p = np.empty((dim, 2, 16))
     M = m(V, C, x, A)
 
-    fig, axs = plt.subplots(5, sharex=True)
+    fig, axs = plt.subplots(dim, sharex=True)
 
     # calculating p
-    for d in range(len(V)):
+    for d in range(dim):
         p[d, :, 0] = np.array([0.5, 0.5])
         for i in range(p.shape[2]-1):       
             p[d, :, i+1] = p[d, :, i] * r(x = p[d, :, i], M = M[:, :, i, d])            
@@ -53,6 +54,53 @@ def main():
     axs[0].set_ylim([0,1])  
     plt.tight_layout()
     plt.show()
+
+"""
+
+def m(M, v, C):
+    #print(M)
+    for i in range(len(v)-1):
+        
+        M[i, 0, 0] = (v[i]- C)/2
+        M[i, 0, 1] = v[i]
+        M[i, 1, 0] = 0
+        M[i, 1, 1] = v[i]/2
+    return M
+
+def main(dim=3):
+    A = [0.1, 1, 10]
+    C = 1.5
+    f = [np.pi/2, np.pi/2, np.pi/2]
+    x = np.linspace(0, 14, 15)
+    V_x = np.empty((3, 15))
+    V = lambda A, x, f: 1 + A * np.sin(f*x)
+    Mat = np.empty((15, 2, 2), dtype=float)
+    P = np.empty((dim, 2, 15))
+    P[:, :, 0] = np.array([0.5, 0.5])
+
+    for i in range(dim):
+        V_x[i] = V(A=A[i], x=x, f=f[i])
+
+    fig, axs = plt.subplots(dim, sharex=True)
+
+    for j in range(dim):
+        Matrix = m(M=Mat, v=V_x[j], C=C)
+        print(Matrix)
+        for k in range(len(x)-1):
+            P[j, :, k+1] = P[j, :, k] * r(P[j, :, k], Matrix[k,:,:])
+
+            P[j, :, k+1] =  P[j, :, k+1]/(sum(np.absolute(P[j, :, k+1]))) 
+        axs[j].plot(x, P[j, 0, :])
+        axs[j].plot(x, P[j, 1, :])
+    plt.show()
+    
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
--- testing_ground.py
+++ testing_ground.py
@@ -1,11 +1,36 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
-C = [1.5 for x in range(5)]
-V = [lambda t: 1 + A * 0.1 * np.sin(t) for A in range(1, 1001, 200)]
 
-x = np.linspace(0, 5, 6)
-print(np.arange(14).reshape(2, 7)[:, 4], sum(np.arange(14).reshape(2, 7)[:, 4]))
-V = [(lambda t, k: 1 + 0.1 * k*  np.sin(t)) for y in range(2)]
-for f, k in zip(V, [x for x in range(len(V))]):
-    print(f(3, k))
+def r(p, M):
+    return M.dot(p)/np.dot(p, M.dot(p))
+f = np.pi/2
+C = 1.5
+A = 1
+V = lambda A, x, f: 1 + A * np.sin(f * x)
 
+x = np.linspace(0, 15, 16)
+M = np.empty(64, dtype=float).reshape((16, 2, 2))# into z, x, y direktion
+P = np.empty((2, 16))
+P[:, 0] = np.array([0.5, 0.5])
+V = V(A, x, f)
+
+
+
+
+
+
+
+
+for i in range(len(x)):
+    M[i, 0, 0] = np.float64((V[i]-C)/2)
+    M[i, 0, 1] = np.float64(V[i])
+    M[i, 1, 0] = 0
+    M[i, 1, 1] = np.float64(V[i]/2)
+
+print(M)
+for i in range(len(x)-1):
+    P[:, i+1] = P[:, i] * r(P[:, i], M[i])
+
+plt.plot(x, P[0], x, P[1])
+plt.show()
